b12502a4b3a769ac7a70d13dbe9461b7a0e28aa1
schandler98 <schandler98@gmail.com>
2022-04-18 21:55:51 -0500

    Update UserService.java

--- FoodBoxApplication/src/main/java/com/FoodBox/service/UserService.java
+++ FoodBoxApplication/src/main/java/com/FoodBox/service/UserService.java
@@ -9,6 +9,7 @@ public interface UserService {
 	
 	List<Users> getAllUsers(); //get a list of all users
 	Users getUserById(Integer userId); // get user by id
+	Users getUserByUsername(String username);
 	Users saveUser(Users user); //save a user
 	void deleteUser(Integer userId);//delete user by userId
 	Users updateUser( Users user ,Integer userId); //update user
