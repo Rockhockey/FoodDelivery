295f388ea5cb75d8ee7d4de6dfae14bf208c2609
schandler98 <95867660+schandler98@users.noreply.github.com>
2022-04-19 14:40:38 -0500

    Add files via upload
    ADDED ORDERS REPO

---
+++ FoodBoxApplication/src/main/java/com/FoodBox/DAO/OrdersRepository.java
@@ -0,0 +1,17 @@
+package com.FoodBox.DAO;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.FoodBox.model.Orders;
+
+public interface OrdersRepository extends JpaRepository<Orders, Integer> {
+	
+	// pull orders by userId
+	
+	
+	//pull orders by Cuisine name
+	
+	
+	//pull ord
+
+}
