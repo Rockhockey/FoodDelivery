af3fdaf1b3ba48224075753b4eee863541cf873f
schandler98 <schandler98@gmail.com>
2022-04-20 02:42:34 -0500

    more methods
    update and delete user CRUD METHOD

--- FoodBoxApplication/src/main/java/com/FoodBox/controller/UserController.java
+++ FoodBoxApplication/src/main/java/com/FoodBox/controller/UserController.java
@@ -6,8 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,6 +47,28 @@ public class UserController {
 	public ResponseEntity<Users> getUserByUsername(@PathVariable("UserName") String userName){
 		return new ResponseEntity<Users>(userService.getUserByUsername(userName), HttpStatus.OK);
 	}
+	
+	// ------------------------------------------- create
+	@PostMapping("/createUser")
+	public ResponseEntity<Users> saveUserEntity(Users user){
+		return new ResponseEntity<Users>(userService.saveUser(user), HttpStatus.CREATED);
+	}
+	
+	// ---------------------------------------- delete
+	
+	@DeleteMapping("{userId}")
+	public ResponseEntity<Void> deleteUser(@PathVariable("userId")Integer userId){
+		userService.deleteUser(userId);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+	
+	// ------------------------------------------ update
+	@PutMapping("{userId}")
+	public ResponseEntity<Users> updateUser(@PathVariable("userId")Integer userId,
+											@RequestBody Users user) {
+		return new ResponseEntity<Users>(userService.updateUser(user, userId), HttpStatus.OK);
+	}
+	
 	
 	
 }
