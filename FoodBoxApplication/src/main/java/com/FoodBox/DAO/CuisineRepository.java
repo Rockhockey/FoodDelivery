88dff32af51abfe115c886c3e693185598028ad5
Marc <56411779+Marc@users.noreply.github.com>
2022-04-19 21:35:04 -0400

    Hashing functions
    Added hashing functions start on input of CRUD

--- FoodBoxApplication/src/main/java/com/FoodBox/DAO/CuisineRepository.java
+++ FoodBoxApplication/src/main/java/com/FoodBox/DAO/CuisineRepository.java
@@ -1,6 +1,7 @@
 package com.FoodBox.DAO;
 
-import java.sql.*;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -21,333 +22,43 @@ public interface CuisineRepository extends JpaRepository<Cuisines, Integer>{
 	
 	-- OUR CUSTOM finder methods go here (filter)
 	*/
-	/*
-	// View menu ordered by Cuisine
-	public static String ViewMenu(Connection con) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer AS CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE Visible=TRUE ORDER BY Cuisine ASC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
+	//View menu ordered by cuisine
+	@Query(Vale="SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer AS CurrentPrice,"
+			+ " Cuisine, Description, Picture FROM Menu WHERE Visible=TRUE ORDER BY Cuisine ASC;", nativeQuery= true)
+	List<Cuisines> ReadMenuOrderCuisine();
+	
+	//View Menu Ordered by Price ASC
+	@Query(Vale="SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer AS CurrentPrice,"
+			+ " Cuisine, Description, Picture FROM Menu WHERE Visible=TRUE ORDER BY CurrentPrice ASC;", nativeQuery= true)
+	List<Cuisines> ReadMenuOrderPriceAsc();
+	
+	//View Menu Ordered by Price Desc
+		@Query(Vale="SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer AS CurrentPrice,"
+				+ " Cuisine, Description, Picture FROM Menu WHERE Visible=TRUE ORDER BY CurrentPrice DESC;", nativeQuery= true)
+		List<Cuisines> ReadMenuOrderPriceDesc();
+	
+	/*View search menu ordered by cuisine
+	 * 	input keyword to search for
+	 */
+	@Query(Vale=" SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu "
+			+ "WHERE(Name LIKE %?1% OR Cuisine=%?1% OR Description LIKE %?1%) AND Visible=TRUE ORDER BY Cuisine ASC;", nativeQuery= true)
+	List<Cuisines> ReadMenuKeywordCuisine (String keyword);
+	
+	/*View search menu ordered by Price ASC
+	 * 	input keyword to search for
+	 */
+	@Query(Vale=" SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu "
+			+ "WHERE(Name LIKE %?1% OR Cuisine=%?1% OR Description LIKE %?1%) AND Visible=TRUE ORDER BY CurrentPrice ASC;", nativeQuery= true)
+	List<Cuisines> ReadMenuKeywordPriceAsc (String keyword);
+	
+	/*View search menu ordered by Price ASC
+	 * 	input cuisine to search for
+	 */
+	@Query(Vale=" SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu "
+			+ "WHERE Cuisine=%?1% AND Visible=TRUE ORDER BY CurrentPrice ASC;", nativeQuery= true)
+	List<Cuisines> ReadMenuCuisinePriceAsc (String keyword);
+	
+	@Query(Vale=" ", nativeQuery= true)
+	List<Cuisines> ReadMenu ();
 
-	// View menu ordered by Price acending
-	public static String ViewMenuPriceAsc(Connection con) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer AS CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE Visible=TRUE ORDER BY CurrentPrice ASC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
-
-	// View menu ordered by Price decending
-	public static String ViewMenuPriceDes(Connection con) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer AS CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE Visible=TRUE ORDER BY CurrentPrice DESC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
-
-	// View menu ordered by Discount Acending
-	public static String ViewMenuDisctAsc(Connection con) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer AS CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE Visible=TRUE ORDER BY PercebtOff ASC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
-
-	// View menu ordered by Discount Descending
-	public static String ViewMenuDisctDesc(Connection con) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer AS CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE Visible=TRUE ORDER BY PercebtOff DESC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
-
-	// search for keyword and View menu ordered by Cuisine Take in String keyword as Param variable	
-	public static String ViewMenuKeywordCuisine(Connection con, String Param) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE(Name LIKE %"+Param+"% OR Cuisine="+Param+"% OR Description LIKE %"+Param+"%) AND Visible=TRUE ORDER BY Cuisine ASC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
-	// search for keyword and View menu ordered by Price Ascending Take in String keyword as Param variable	
-	public static String ViewMenuKeywordPriceAsc(Connection con, String Param) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE(Name LIKE %"+Param+"% OR Cuisine="+Param+"% OR Description LIKE %"+Param+"%) AND Visible=TRUE ORDER BY Price ASC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
-
-	// search for keyword and View menu ordered by Price Descending Take in String keyword as Param variable	
-	public static String ViewMenuKeywordPriceDesc(Connection con, String Param) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE(Name LIKE %"+Param+"% OR Cuisine="+Param+"% OR Description LIKE %"+Param+"%) AND Visible=TRUE ORDER BY Price DESC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
-
-	// search for keyword and View menu ordered by Discount Ascending Take in String keyword as Param variable
-	public static String ViewMenuKeywordDisctAsc(Connection con, String Param) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE(Name LIKE %"+Param+"% OR Cuisine="+Param+"% OR Description LIKE %"+Param+"%) AND Visible=TRUE ORDER BY PercentOff ASC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
-
-	// search for keyword and View menu ordered by Discount Descending Take in String keyword as Param variable
-	public static String ViewMenuKeywordDisctDesc(Connection con, String Param) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE(Name LIKE %"+Param+"% OR Cuisine="+Param+"% OR Description LIKE %"+Param+"%) AND Visible=TRUE ORDER BY PercentOff DESC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr + "\n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
-
-	//View 1 cuisine on menu Take in String Cuisine as Param variable
-	public static String ViewMenuCuisine(Connection con, String Param) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE Cuisine="+Param+" AND Visible=TRUE";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
-
-	//View 1 cuisine on menu ordered by price Ascending Take in String Cuisine as Param variable
-	public static String ViewMenuCuisinePriceAsc(Connection con, String Param) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE Cuisine="+Param+" AND Visible=TRUE ORDER BY Price ASC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
-
-	//View 1 cuisine on menu ordered by price Descending Take in String Cuisine as Param variable
-	public static String ViewMenuCuisinePriceDesc(Connection con, String Param) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE Cuisine="+Param+" AND Visible=TRUE ORDER BY Price DESC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
-
-	//View 1 cuisine on menu ordered by Discount Ascending Take in String Cuisine as Param variable
-	public static String ViewMenuCuisineDisctAsc(Connection con, String Param) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE Cuisine="+Param+" AND Visible=TRUE ORDER BY PercentOff ASC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}
-
-	//View 1 cuisine on menu ordered by Discount Descending Take in String Cuisine as Param variable	
-	public static String ViewMenuCuisineDisctDesc(Connection con, String Param) throws SQLException {
-		String query = "SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu WHERE Cuisine="+Param+" AND Visible=TRUE ORDER BY PercentOff DESC";
-		try (Statement stmt = con.createStatement()) {
-		  ResultSet rs = stmt.executeQuery(query);
-		  String result = "";
-		  while (rs.next()) {
-			String name = rs.getString("Name");
-			float price = rs.getFloat("Price");
-			float discount = rs.getFloat("PercentOff");
-			float CurrentPrice= rs.getFloat("CurrentPrice");
-			String Cuisine = rs.getString("Cuisine");
-			String Descr = rs.getString("Description");
-			String ImageLink = rs.getString("Picture");
-			result= result + name + "," + price + "," + discount + "," + CurrentPrice + "," + Cuisine + "," + Descr +","+ImageLink+"/n";
-		  }
-		  return result;
-		} catch (SQLException e) {
-		  JDBCTutorialUtilities.printSQLException(e);
-		}
-	}	*/
 }
