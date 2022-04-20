da58ce9f0dfe1ff0378998a9e6875f75ca5f9de6
schandler98 <schandler98@gmail.com>
2022-04-20 02:46:52 -0500

    updated annotations

--- FoodBoxApplication/src/main/java/com/FoodBox/controller/CuisineController.java
+++ FoodBoxApplication/src/main/java/com/FoodBox/controller/CuisineController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.FoodBox.model.Cuisines;
 import com.FoodBox.service.CuisinesService;
 
-@Controller
+@RestController
 @RequestMapping("/foods")
 public class CuisineController {
 	
