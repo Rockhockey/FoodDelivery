cc4b84d8569058729fb30518f305577c5f002fce
Marc <56411779+Marc@users.noreply.github.com>
2022-04-26 14:18:42 -0400

    Update CuisineRepository.java
    Fix to work with html

--- FoodBoxApplication/src/main/java/com/FoodBox/DAO/CuisineRepository.java
+++ FoodBoxApplication/src/main/java/com/FoodBox/DAO/CuisineRepository.java
@@ -1,12 +1,16 @@
 package com.FoodBox.DAO;
 
+import java.sql.*;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import com.FoodBox.model.Cuisines;
-import org.springframework.data.repository.query.*;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import com.FoodBox.model.Cuisines;
+
+@Repository
 public interface CuisineRepository extends JpaRepository<Cuisines, Integer>{
 
 	
@@ -22,34 +26,22 @@ public interface CuisineRepository extends JpaRepository<Cuisines, Integer>{
 	-- OUR CUSTOM finder methods go here (filter)
 	*/
 	
-	
-	 
-	 
 	//View menu ordered by cuisine
-	@Query(value="SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer AS CurrentPrice,"
-			+ " Cuisine, Description, Picture FROM Menu WHERE Visible=TRUE ORDER BY Cuisine ASC;", nativeQuery= true)
+	@Query(value="SELECT * FROM menu WHERE visible=TRUE ORDER BY cuisine_type ASC;", nativeQuery= true)
 	List<Cuisines> ReadMenuOrderCuisine();
 	
 	//View Menu Ordered by Price ASC
-	@Query(value="SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer AS CurrentPrice,"
-			+ " Cuisine, Description, Picture FROM Menu WHERE Visible=TRUE ORDER BY CurrentPrice ASC;", nativeQuery= true)
+	@Query(value="SELECT *, price*offer AS CurrentPrice FROM menu WHERE visible=TRUE ORDER BY CurrentPrice ASC;", nativeQuery= true)
 	List<Cuisines> ReadMenuOrderPriceAsc();
 	
 	//View Menu Ordered by Price Desc
-		@Query(value="SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer AS CurrentPrice,"
-				+ " Cuisine, Description, Picture FROM Menu WHERE Visible=TRUE ORDER BY CurrentPrice DESC;", nativeQuery= true)
+		@Query(value="SELECT *, price*offer AS CurrentPrice FROM menu WHERE visible=TRUE ORDER BY CurrentPrice DESC;", nativeQuery= true)
 		List<Cuisines> ReadMenuOrderPriceDesc();
-		
-	//View Menu Ordered by Discount Desc
-		@Query(value="SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer AS CurrentPrice,"
-				+ " Cuisine, Description, Picture FROM Menu WHERE Visible=TRUE ORDER BY PercentOff DESC;", nativeQuery= true)
-		List<Cuisines> ReadMenuOrderDiscountDesc();
 	
 	/*View search menu ordered by cuisine
 	 * 	input keyword to search for
 	 */
-	@Query(value=" SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu "
-			+ "WHERE(Name LIKE %:keyword1% OR Cuisine=%:keyword2% OR Description LIKE %:keyword3%) AND Visible=TRUE ORDER BY Cuisine ASC;", nativeQuery= true)
+	@Query(value=" SELECT * FROM menu WHERE(name LIKE %:keyword1% OR cuisine_type=%:keyword2% OR description LIKE %:keyword3%) AND visible=TRUE ORDER BY cuisine_type ASC;", nativeQuery= true)
 	List<Cuisines> ReadMenuKeywordCuisine (
 			@Param("keyword1") String keyword1,
 			@Param("keyword2") String keyword2,
@@ -58,8 +50,8 @@ public interface CuisineRepository extends JpaRepository<Cuisines, Integer>{
 	/*View search menu ordered by Price ASC
 	 * 	input keyword to search for
 	 */
-	@Query(value=" SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu "
-			+ "WHERE(Name LIKE %:keyword1% OR Cuisine=%:keyword2% OR Description LIKE %:keyword3%) AND Visible=TRUE ORDER BY CurrentPrice ASC;", nativeQuery= true)
+	@Query(value=" SELECT *, price*offer as CurrentPrice FROM menu WHERE(mame LIKE %:keyword1% OR cuisine=%:keyword2% OR description LIKE %:keyword3%)"
+		+ " AND visible=TRUE ORDER BY CurrentPrice ASC;", nativeQuery= true)
 	List<Cuisines> ReadMenuKeywordPriceAsc (
 			@Param("keyword1") String keyword1,
 			@Param("keyword2") String keyword2,
@@ -68,46 +60,27 @@ public interface CuisineRepository extends JpaRepository<Cuisines, Integer>{
 	/*View search menu ordered by Price Desc
 	 * 	input keyword to search for
 	 */
-	@Query(value=" SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu "
-			+ "WHERE(Name LIKE %:keyword1% OR Cuisine=%:keyword2% OR Description LIKE %:keyword3%) AND Visible=TRUE ORDER BY CurrentPrice DESC;", nativeQuery= true)
+	@Query(value=" SELECT *, price*offer as CurrentPrice FROM menu WHERE(mame LIKE %:keyword1% OR cuisine=%:keyword2% OR description LIKE %:keyword3%)"
+		+ " AND visible=TRUE ORDER BY CurrentPrice DESC;", nativeQuery= true)
 	List<Cuisines> ReadMenuKeywordPriceDesc (
 			@Param("keyword1") String keyword1,
 			@Param("keyword2") String keyword2,
 			@Param("keyword3") String keyword3);
 	
-	/*View search menu ordered by Discount Desc
-	 * 	input keyword to search for
-	 */	
-	@Query(value=" SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu "
-			+ "WHERE(Name LIKE %:keyword1% OR Cuisine=%:keyword2% OR Description LIKE %:keyword3%) AND Visible=TRUE ORDER BY PercentOff DESC;", nativeQuery= true)
-	List<Cuisines> ReadMenuKeywordDiscountDesc(
-	@Param("keyword1") String keyword1,
-	@Param("keyword2") String keyword2,
-	@Param("keyword3") String keyword3);
-	
 	/*View search menu ordered by Price ASC
 	 * 	input cuisine to search for
 	 */
-	@Query(value=" SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu "
-			+ "WHERE Cuisine=%:foodType% AND Visible=TRUE ORDER BY CurrentPrice ASC;", nativeQuery= true)
+	@Query(value=" SELECT *, price*offer as CurrentPrice FROM menu "
+			+ "WHERE cuisine_type LIKE %:foodType% AND visible=TRUE ORDER BY CurrentPrice ASC;", nativeQuery= true)
 	List<Cuisines> ReadMenuCuisinePriceAsc (
 			@Param("foodType") String cuisine);
-	
+
 	/*View search menu ordered by Price Desc
 	 * 	input cuisine to search for
 	 */
-	@Query(value=" SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu "
-			+ "WHERE Cuisine=%:foodType% AND Visible=TRUE ORDER BY CurrentPrice DESC;", nativeQuery= true)
+	@Query(value=" SELECT *, price*offer as CurrentPrice FROM menu "
+			+ "WHERE cuisine_type LIKE %:foodType% AND visible=TRUE ORDER BY CurrentPrice DESC;", nativeQuery= true)
 	List<Cuisines> ReadMenuCuisinePriceDesc (
 			@Param("foodType") String cuisine);
-	
-	/*View search menu ordered by Discount Desc
-	 * 	input cuisine to search for
-	 */
-	@Query(value=" SELECT Name, Price, (1-Offer)*100 AS PercentOff, Price*Offer as CurrentPrice, Cuisine, Description, Picture FROM Menu "
-			+ "WHERE Cuisine=%:foodType% AND Visible=TRUE ORDER BY PercentOff DESC;", nativeQuery= true)
-	List<Cuisines> ReadMenuCuisineDiscountDesc (
-			@Param("foodType") String cuisine);
-
 
 }
