68b889a38768d608e8a4de04f44029e91eceadc5
schandler98 <schandler98@gmail.com>
2022-04-27 16:20:01 -0500

    cart delete mapping
    cart controller -- made delete mapping for deleting a cart by cart_index

--- FoodBoxApplication/src/main/java/com/FoodBox/controller/CartController.java
+++ FoodBoxApplication/src/main/java/com/FoodBox/controller/CartController.java
@@ -4,6 +4,8 @@ package com.FoodBox.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,8 +50,13 @@ public class CartController {
 	}
 	
 		//RESTful API for Update operation
+	
 		
 		//RESTful API for Delete operation
-	
+	@DeleteMapping("{cart_index}")
+	public ResponseEntity<Void> deleteCart(@PathVariable("cart_index") Integer cart_index){
+		cartService.deleteCart(cart_index);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
 	
 }
