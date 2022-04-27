d48b9e894c1eb4128c88f9e1c06a1593be337650
schandler98 <schandler98@gmail.com>
2022-04-27 16:17:43 -0500

    cart controller
    made cart controller

--- FoodBoxApplication/src/main/java/com/FoodBox/controller/CartController.java
+++ FoodBoxApplication/src/main/java/com/FoodBox/controller/CartController.java
@@ -2,11 +2,17 @@ package com.FoodBox.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.FoodBox.DAO.CartRepository;
 import com.FoodBox.DAO.CuisineRepository;
+import com.FoodBox.model.Cart;
+import com.FoodBox.model.Cuisines;
 import com.FoodBox.service.CartService;
 
 
@@ -14,16 +20,33 @@ import com.FoodBox.service.CartService;
 @RequestMapping("/cart")
 public class CartController {
 
+	@Autowired
 	CartRepository cartRepo;
+	
+	@Autowired
 	CuisineRepository cuisineRepo;
 	
 	@Autowired
 	private CartService cartService;
+
+	public CartController(CartRepository cartRepo, CuisineRepository cuisineRepo, CartService cartService) {
+		super();
+		this.cartRepo = cartRepo;
+		this.cuisineRepo = cuisineRepo;
+		this.cartService = cartService;
+	}
+	
+	
 	
 		//RESTful API for Retrieval operations
 	
 		//RESTful API for Create operations
-		
+	
+	@PostMapping("/new_cart")
+	public ResponseEntity<Cart> saveCart(@RequestBody Cart cart){
+		return new ResponseEntity<Cart>(cartService.saveCart(cart), HttpStatus.CREATED);
+	}
+	
 		//RESTful API for Update operation
 		
 		//RESTful API for Delete operation
